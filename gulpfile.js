233cf4fb4ec3e260abb78aa661441a92defc6473
Charles Fulton <fultonc@lafayette.edu>
2014-08-04 15:43:24 -0400

    add fontfix task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp    = require('gulp'),
     jshint  = require('gulp-jshint'),
     phplint = require('phplint'),
+    replace = require('gulp-replace'),
     less    = require('gulp-less');
 
 // PHP lint theme files.
@@ -12,6 +13,10 @@ gulp.task('phplint', function() {
 gulp.task('styles', function() {
   return gulp.src('./less/stellar.less')
     .pipe(less())
+    .pipe(replace('glyphicons-halflings-regular.eot','glyphicons-halflings-regular.eot]]'))
+    .pipe(replace('glyphicons-halflings-regular.svg','glyphicons-halflings-regular.svg]]'))
+    .pipe(replace('glyphicons-halflings-regular.ttf','glyphicons-halflings-regular.ttf]]'))
+    .pipe(replace('glyphicons-halflings-regular.woff','glyphicons-halflings-regular.woff]]'))
     .pipe(gulp.dest('./style/'));
 });
 
