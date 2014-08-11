0e34e44f3a8c67c845c26d77a28859ea6f0b8fcc
Charles Fulton <fultonc@lafayette.edu>
2014-08-11 09:59:24 -0400

    Add chooser string

--- lang/en/theme_stellar.php
+++ lang/en/theme_stellar.php
@@ -22,6 +22,16 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['choosereadme'] = '
+<div class="clearfix"><div class="theme_screenshot"><h2>Stellar</h2>
+<img class=img-polaroid src="stellar/pix/screenshot.png" />
+<h3>About</h3>
+<p>This is a child theme of Bootstrap for Moodle. Its design is based on the earlier fixed-width Cosmic and Rocket themes. It was developed by Lafayette College ITS.</p>
+<p>
+Authors: Charles Fulton, Jason Alley, Erwin Annulyse
+</p>
+<h3>Reporting bugs</h3>
+<p>Please report bugs on the <a href="https://github.com/LafColITS/theme_stellar/issues">github page</a> for this theme.</p>';
 $string['configtitle'] = 'Stellar theme';
 $string['logo'] = 'Logo';
 $string['logodesc'] = 'Enter the URL to an image to use as the logo for this site. Should be http://www.yoursite.com/path/to/logo.png';
